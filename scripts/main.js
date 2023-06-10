5c20b1528adcebbb877a93ac19c7fb72999c262c
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-09 23:18:55 -0400

    separated grid reset and creation into functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,49 @@
-const GRID_SIZE = 4;
-const GRID_HEIGHT = '400px';
-
+// node-reference variables
 const container = document.querySelector('.container');
-
+const resizeButton = document.querySelector('.resize-button');
 const resetButton = document.querySelector('.reset-button');
-resetButton.addEventListener('click', () =>{
-    const boxes = document.querySelectorAll('.grid-box')
-    boxes.forEach(box => box.classList.remove('colored'));
-})
+
+// creation of wrapper for grid that is 'gridSize' boxes across and 'Grid_Height' px tall/wide
+const GRID_HEIGHT = '400px';
+let gridSize = 4;
 
 const wrapper = document.createElement('div');
 wrapper.style.height = wrapper.style.width = GRID_HEIGHT;
 wrapper.classList.add('grid-wrapper');
 container.insertBefore(wrapper, resetButton);
 
-// calculate the dimensions of each box by using the width of grid wrapper
-let boxHeight = wrapper.clientWidth / GRID_SIZE;
-// Loop the creation of boxes; number = grid dimension ^ 2
-
-
-
-for (let boxes = 0; boxes < GRID_SIZE ** 2; boxes++)
+// generates the the boxes that make of the grid
+// box dimensions and height accommodate 'GRID_HEIGHT' and 'gridSize'
+createBoxes();
+function createBoxes()
 {
-    let box = document.createElement('div');
-    box.classList.add('grid-box');
-    box.style.height = box.style.width = boxHeight + 'px';
-
-    box.addEventListener('mouseover', () => box.classList.add('colored'))
-    box.addEventListener('click', (e) => e.target.classList.toggle('colored'));
-    wrapper.appendChild(box);
+    let boxHeight = wrapper.clientWidth / gridSize;
+    for (let boxes = 0; boxes < gridSize ** 2; boxes++)
+    {
+        let box = document.createElement('div');
+        box.classList.add('grid-box');
+        box.style.height = box.style.width = boxHeight + 'px';
+    
+        box.addEventListener('mouseover', () => box.classList.add('colored'))
+        box.addEventListener('click', (e) => e.target.classList.toggle('colored'));
+        wrapper.appendChild(box);
+    }
 }
-// for each box, add grid-box to classlist
 
-// append each box to the grid as child
+// resize button to create a new grid
+// number of boxes across will come from user input
+resizeButton.addEventListener('click', resizeGrid)
+
+function resizeGrid(){
+    
+}
+
+// reset button to clear boxes
+resetButton.addEventListener('click', resetBoxes)
+
+function resetBoxes()
+{
+    const boxes = document.querySelectorAll('.grid-box')
+    boxes.forEach(box => box.classList.remove('colored'));
+}
+
